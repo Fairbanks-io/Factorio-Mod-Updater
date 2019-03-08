120b05d24059473b04b37c42bc124d635155d1e0
Jon Fairbanks <fairplay89@gmail.com>
2019-03-07 22:05:16 -0800

    Error Handling

--- factorio-mod-updater.sh
+++ factorio-mod-updater.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # Colors
+RED='\033[0;31m';
 GREEN='\033[0;32m';
 NC='\033[0m';
 
-readarray -t arr < <(jq -c '.[] | .[] | .name' mod-list.json | tr -d \");
-total=$(jq -r ".[]" mod-list.json | jq length);
-count=1; # Start at 1 since "base" won't ever need downloaded
+dir=$(pwd)
+if [[ -e "$dir/mods/mod-list.json" ]]; then
+	readarray -t arr < <(jq -c '.[] | .[] | .name' $dir/mods/mod-list.json | tr -d \");
+	total=$(jq -r ".[]" $dir/mods/mod-list.json | jq length);
+	count=0;
+else
+	echo -e "${RED}mods/mod-list.json not found${NC}";
+	sleep 2;
+	exit;
+fi
 
 echo "Downloading $total Factorio Mods listed in mod-list.json...";
 sleep 2;
@@ -14,19 +22,22 @@ sleep 2;
 mkdir -p mods;
 
 for mod in "${arr[@]}"; 
-	do 
-		echo "Found: $mod";
-		base_url="https://mods.factorio.com/api/mods/$mod";
-		download_url=$(curl --silent $base_url | jq -r '.releases | .[0].download_url');
-		file_name=$(curl --silent $base_url | jq -r '.releases | .[0].file_name');
-		if [ "$download_url" != "null" ]
-		then
-			wget -nc --quiet "https://mods.factorio.com/$download_url" -O "mods/$file_name";
-			echo "Downloaded: $file_name";
-			count=$((count + 1));
-		fi
-	done
+do 
+	echo "Found: $mod";
+	base_url="https://mods.factorio.com/api/mods/$mod";
+	download_url=$(curl --silent $base_url | jq -r '.releases | .[0].download_url');
+	file_name=$(curl --silent $base_url | jq -r '.releases | .[0].file_name');
+	if [ "$download_url" != "null" ]
+	then
+		wget -bqc -nc --quiet "https://mods.factorio.com/$download_url" -O "mods/$file_name" > /dev/null 2>&1;
+		echo "Downloaded: $file_name";
+		count=$((count + 1));
+	else
+		echo -e "${RED}Unable to find a suitable download for $mod${NC}"
+	fi
+done
 
-echo -e "${GREEN}[Success]${NC} $count of $total Factorio Mods Downloaded!";
+sleep 2;
+echo -e "${GREEN}[Done]${NC} $count of $total Factorio Mods Downloaded!";
 
 exit;
